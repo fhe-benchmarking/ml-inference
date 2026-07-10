932e607cb99aaab7030494ef6c304846492708b8
ngesbrian <nges.brian@gmail.com>
2026-07-10 14:38:14 -0700

    removing a duplicated bootstrap in model:

--- submissions/cifar10/src/resnet20_fheon.cpp
+++ submissions/cifar10/src/resnet20_fheon.cpp
@@ -219,8 +219,7 @@ Ctext resnet_block(FHEONHEController &fheonHEController, FHEONANNController &fhe
     if (bootstrapState) {
         convData = fheonHEController.bootstrap_function(convData);
     }
-
-    convData = fheonHEController.bootstrap_function(convData);
+    
     convData = fheonANNController.he_relu(convData, reluScale, dataSize, polyDeg);
     Ctext secConvData = convolution_block(fheonHEController, fheonANNController, layer + "_conv2", convData,
                                     dataWidth, kernelWidth, striding, outputChannels, outputChannels);
